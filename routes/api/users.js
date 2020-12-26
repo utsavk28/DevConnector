724828c1bec9177178ed8542abdad7e5957ca040
Utsav Khatu <utsavkhatu416@gmail.com>
2020-12-26 12:40:52 +0530

    Auth & user Routes Added

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const gravatar = require("gravatar")
 const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
+const config = require("config")
 const { check, validationResult } = require("express-validator")
 
 const User = require('../../models/User')
@@ -23,32 +25,42 @@ router.post("/", [
         const { name, email, password } = req.body
 
         try {
-            let user = await User.findOne({email})
+            let user = await User.findOne({ email })
 
             // See if user exists
-            if(user) {
-                return res.status(400).json({errors:[{msg:'User already exists'}]})
+            if (user) {
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
             }
-            
+
             // Get user gravatar
             const avatar = gravatar.url(email, {
-                s:'200',
-                r:'pg',
-                d:'mm'
+                s: '200',
+                r: 'pg',
+                d: 'mm'
             })
 
             user = new User({
-                name,email,avatar,password
+                name, email, avatar, password
             })
             // Encrypt password
             const salt = await bcrypt.genSalt(10)
 
-            user.password = await bcrypt.hash(password,salt)
+            user.password = await bcrypt.hash(password, salt)
             await user.save()
 
             // Return jsonwebtoken
-            res.send("User Registered")
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
+            jwt.sign(payload, config.get('jwtSecret'), {
+                expiresIn: 360000
+            }, (err, token) => {
+                if (err) throw err;
+                res.json({ token })
+            })
         }
         catch (err) {
             console.error(err.message);
