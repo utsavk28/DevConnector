6d6d60547b53efa17ea6122ee1d1332af5dd2ca0
Utsav Khatu <utsavkhatu416@gmail.com>
2020-12-28 18:51:24 +0530

    All Profile route Added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,44 @@
 const express = require("express")
 const router = express.Router()
+const auth = require("../../middleware/auth")
+const { check, validationResult } = require("express-validator")
 
-// @route       GET api/posts
-// @description Test route
-// @access      Public
-router.get("/",(req,res) => {
-    res.send("Posts Route")
-})
+const Post = require("../../models/Post")
+const Profile = require("../../models/Profile")
+const User = require("../../models/User")
+
+// @route       POST api/posts
+// @description Create a post
+// @access      Private
+router.post("/", [auth,
+    [
+        check('text', 'Text is required').not().isEmpty()
+    ]
+],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password')
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            })
+
+            const post = await newPost.save()
+            res.json(post)
+        } catch (error) {
+            console.error(error.message);
+        }
+
+
+    })
+
+
 
 module.exports = router
